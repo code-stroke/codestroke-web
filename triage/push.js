7375f901122b31049b43e12075d7ce0dec71fc36
Daniel Shell <dshell321@gmail.com>
2018-04-19 12:33:35 +1000

    Push Notifications integrated into Triage app
    - Also, added jquery as a local file

---
+++ triage/push.js
@@ -0,0 +1,154 @@
+/* Push Notification Handler */
+
+const DOM_Push = {};
+//const applicationServerPublicKey = 'BM2e4iaBRYGSs5I_jXJaXLY6p5_koeOKXK0MbPEsoTIpyrj0rZx3rDUtZ05ueOqn-Cl6W5HrxonQUHlUZtwJA9I';
+const applicationServerPublicKey = 'BNp4vAfivAwb1BYLFV6ZC-Qd0hbxPZ2LhS9H_WUOCPIEtIUzQDqfTlYLQtHj6v3MOVd6v2obn3VOQLswCvYBZlw';
+
+let isSubscribed = false;
+let swRegistration = null;
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+function initializeUI() {
+    DOM_Push.button.children().removeClass("disabled");
+
+    DOM_Push.button.click(function() {
+        if (isSubscribed) {
+          unsubscribeUser();
+        } else {
+          subscribeUser();
+        }
+    });
+
+    // Set the initial subscription value
+    swRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+        isSubscribed = !(subscription === null);
+
+        updateSubscriptionOnServer(subscription);
+
+        if (isSubscribed) {
+            console.log('User IS subscribed.');
+        } else {
+            console.log('User is NOT subscribed.');
+        }
+
+        updateBtn();
+    });
+}
+
+function updateBtn() {
+    if (Notification.permission === 'denied') {
+        console.log("Push messaging blocked by browser!");
+        disableBtn("Error: Push Messaging blocked. Please contact IT staff");
+        updateSubscriptionOnServer(null);
+        return;
+    }
+
+    if (isSubscribed) {
+        DOM_Push.status.text("On");
+        DOM_Push.status.addClass("on");
+        DOM_Push.status.removeClass("off");
+    } else {
+        DOM_Push.status.text("Off");
+        DOM_Push.status.addClass("off");
+        DOM_Push.status.removeClass("on");
+    }
+}
+
+function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+    })
+    .then(function(subscription) {
+        console.log('User is subscribed.');
+
+        updateSubscriptionOnServer(subscription);
+
+        isSubscribed = true;
+
+        updateBtn();
+    })
+    .catch(function(err) {
+        console.log('Failed to subscribe the user: ', err);
+        updateBtn();
+    });
+}
+
+function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server, for now will just print subscription
+    DOM_Push.debug.text(JSON.stringify(subscription));
+}
+
+function unsubscribeUser() {
+    swRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+        if (subscription) {
+            return subscription.unsubscribe();
+        }
+    })
+    .catch(function(error) {
+        console.log('Error unsubscribing', error);
+    })
+    .then(function() {
+        updateSubscriptionOnServer(null);
+
+        console.log('User is unsubscribed.');
+        isSubscribed = false;
+
+        updateBtn();
+    });
+}
+
+function disableBtn(message) {
+    DOM_Push.status.text("Disabled");
+    DOM_Push.status.removeClass("on off");
+
+    DOM_Push.button.prop("title", message);
+    DOM_Push.button.children().addClass("disabled");
+
+    DOM_Push.button.off();
+}
+
+$( document ).ready(function() {
+    DOM_Push.button = $("#js-push-button");
+    DOM_Push.status = DOM_Push.button.find(".push_status");
+    DOM_Push.debug = $("#js-push-debug");
+
+    /*register service worker and enable button if browser supports push notifications*/
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+        console.log('Service Worker and Push is supported');
+
+        navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+            console.log('Service Worker registered', swReg);
+
+            swRegistration = swReg;
+            initializeUI() ;
+        })
+        .catch(function(error) {
+            disableBtn("Error: Error enabling push notifications. Please contact IT staff");
+            console.error('Service Worker Error', error);
+        });
+
+    } else {
+        disableBtn("Error: Push Messaging not supported in your current browser");
+        console.warn('Push messaging is not supported');
+    }
+
+});
---
+++ triage/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push CodeStroke';
+  const options = {
+    body: 'Test successful.'
+  };
+
+  const notificationPromise = self.registration.showNotification(title, options);
+
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+/* change as needed */
+    clients.openWindow('http://www.austin.org.au/')
+  );
+});
