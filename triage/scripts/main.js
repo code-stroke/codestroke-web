5ed23f5defc5cb9ce15208d7ef312f0d44d6a69c
wjm07 <utenacasshern@gmail.com>
2018-04-21 01:53:03 +0200

    login page and barebones functionality added

---
+++ triage/scripts/main.js
@@ -0,0 +1,13 @@
+// Local server testing only
+var localserver = "http://localhost:5000/";
+
+$(document).ready(function(){
+    var user_name = Cookies.get("user_name");
+    if (user_name !== undefined) {
+	$(".sidebar-user-name span").text(Cookies.get("user_name"));
+    } else {
+	console.log("Not logged in");
+	window.location.replace("/triage/login.html");
+    }
+});
+
