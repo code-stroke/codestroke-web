be2817d57a2c9b5ef3329ce3a92f6cf2b1720d46
Daniel Shell <dshell321@gmail.com>
2018-05-28 19:04:01 +1000

    Added Assessment page with (smooth) anchor links

--- v1/scripts/case.js
+++ v1/scripts/case.js
@@ -1,3 +1,5 @@
+const DOM_Assess = {};
+
 function loadInputs() {
     $(".js-input").change(function() {
         if ($(".js-input").val() != "") {
@@ -8,11 +10,45 @@ function loadInputs() {
     })
 }
 
+function loadAssessLinks() {
+    DOM_Assess["btns"].click(function() {
+        let loc = $(this).data("anchor");
+        let val = DOM_Assess[loc].offset().top - DOM_Assess["section"].offset().top + DOM_Assess["section"].scrollTop();
+
+        DOM_Assess["section"].animate({
+            scrollTop: val
+        }, {
+            duration: 500,
+            easing: "swing"
+        })
+
+    });
+}
+
+function loadDOM() {
+    let assess = {
+        section: $("#js-assess-section"),
+        btns: $(".js-assess-button"),
+        mass: $("#js-assess-mass"),
+        vitals: $("#js-assess-vitals"),
+        race: $("#js-assess-race"),
+        cannula: $("#js-assess-cannula"),
+        nihss: $("#js-assess-nihss"),
+        mrs: $("#js-assess-mrs"),
+        submit: $("#js-assess-submit")
+    }
+    $.extend(DOM_Assess, assess);
+}
+
 /************
  * ON READY *
  ************/
 
 $(document).ready(function() {
+    loadDOM();
+
+    loadAssessLinks();
+
     loadInputs();
 });
 
