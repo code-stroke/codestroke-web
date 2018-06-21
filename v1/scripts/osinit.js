a6d32f82a4c1ea98c7d15d2bf1eb7ab656a5a24d
mphan365 <37003365+mphan365@users.noreply.github.com>
2018-06-21 16:53:52 +1000

    Debug
    removed script tags from init file

--- v1/scripts/osinit.js
+++ v1/scripts/osinit.js
@@ -1,8 +1,8 @@
-<script>
-  var OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
+//OneSignal init//
+
+var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
     OneSignal.init({
       appId: "a704a88e-9e37-41f6-99b8-6ded41926c03",
     });
-  });
-</script>
+});
