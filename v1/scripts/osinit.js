d991fbb9c1bf7a4acbe5a91e309bbfb29e4e323a
mphan365 <37003365+mphan365@users.noreply.github.com>
2018-06-22 00:37:26 +1000

    Code clean up
    OneSignal APIs now integrated with our user interface
    
    removed unecessary code

--- v1/scripts/osinit.js
+++ v1/scripts/osinit.js
@@ -1,8 +1,12 @@
-//OneSignal init//
+//OneSignal initialiser//
 
 var OneSignal = window.OneSignal || [];
-    OneSignal.push(function() {
-    OneSignal.init({
-      appId: "a704a88e-9e37-41f6-99b8-6ded41926c03",
-    });
+OneSignal.push(function() {
+  OneSignal.init({
+    appId: "a704a88e-9e37-41f6-99b8-6ded41926c03",
+    autoRegister: false,
+    notifyButton: {
+      enable: false,
+    },
+  });
 });
--- v1/scripts/push-os.js
+++ v1/scripts/push-os.js
@@ -5,8 +5,6 @@ const DOM_Push = {};
 const TEMP_Push = {}
 TEMP_Push.button = ({ status }) => `Notifications &nbsp <img src="icons/button/switch-${status}.png" />`;
 
-
-
 function initializeUI() {
     DOM_Push.button.removeClass("disabled");
 
@@ -50,71 +48,6 @@ function getSubscriptionState() {
     });
 }
 
-
-
-
-
-function updateBtn() {
-    if (Notification.permission === 'denied') {
-        console.log("Push messaging blocked by browser!");
-        disableBtn("Error: Push Messaging blocked. Please contact IT staff");
-        updateSubscriptionOnServer(null);
-        return;
-    }
-
-    if (isSubscribed) {
-        DOM_Push.button.html(TEMP_Push.button({status: "on"}));
-    } else {
-        DOM_Push.button.html(TEMP_Push.button({status: "off"}));
-    }
-
-    $("body").removeClass("loading");
-}
-
-function subscribeUser() {
-    $("body").addClass("loading");
-
-    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-    swRegistration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: applicationServerKey
-    })
-    .then(function(subscription) {
-        console.log('User is subscribed.');
-
-        updateSubscriptionOnServer(subscription);
-
-        isSubscribed = true;
-
-        updateBtn();
-    })
-    .catch(function(err) {
-        console.log('Failed to subscribe the user: ', err);
-        updateBtn();
-    });
-}
-
-
-function unsubscribeUser() {
-    swRegistration.pushManager.getSubscription()
-    .then(function(subscription) {
-        if (subscription) {
-            return subscription.unsubscribe();
-        }
-    })
-    .catch(function(error) {
-        console.log('Error unsubscribing', error);
-    })
-    .then(function() {
-        updateSubscriptionOnServer(null);
-
-        console.log('User is unsubscribed.');
-        isSubscribed = false;
-
-        updateBtn();
-    });
-}
-
 function disableBtn(message) {
     DOM_Push.button.html(TEMP_Push.button({status: "disabled"}));
     DOM_Push.button.addClass("disabled");
@@ -126,7 +59,6 @@ function disableBtn(message) {
 
 $( document ).ready(function() {
     DOM_Push.button = $("#js-push-button");
-    //DOM_Push.debug = $("#js-push-debug");
 
 /*register service worker and enable button if browser supports push notifications*/
     if ('serviceWorker' in navigator && 'PushManager' in window) {
