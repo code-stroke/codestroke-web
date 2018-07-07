93ddc47478459de01ffdfaf207686ee65d43041c
mphan365 <37003365+mphan365@users.noreply.github.com>
2018-07-08 01:22:16 +1000

    debug
    init previously called twice

--- v1/scripts/push-os.js
+++ v1/scripts/push-os.js
@@ -1,16 +1,3 @@
-//OneSignal initialiser//
-
-var OneSignal = window.OneSignal || [];
-OneSignal.push(function() {
-  OneSignal.init({
-    appId: "a704a88e-9e37-41f6-99b8-6ded41926c03",
-    autoRegister: true,
-    notifyButton: {
-      enable: false,
-    },
-  });
-});
-
 /* Push Notification Handler */
 
 const DOM_Push = {};
