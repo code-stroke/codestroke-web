6ae62f29c3c61fdfda1b8180187fd6f51df37fe3
mphan365 <37003365+mphan365@users.noreply.github.com>
2018-07-27 21:32:36 +1000

    SendTags added to init code

--- v1/scripts/osinit.js
+++ v1/scripts/osinit.js
@@ -22,3 +22,11 @@ if (OneSignal.installServiceWorker) {
     navigator.serviceWorker.register('/OneSignalSDKWorker.js?appId=a704a88e-9e37-41f6-99b8-6ded41926c03');
   }
 }
+
+OneSignal.sendTags({
+   key: API.login.signoff.signoff_role,
+
+ }).then(function(tagsSent) {
+   console.log('tag set as ' API.login.signoff.signoff_role)
+ });
+});
