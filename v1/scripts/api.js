2e2c2d4b5a389bfbf7ef874598be7f2d2ed3fd4d
mphan365 <37003365+mphan365@users.noreply.github.com>
2018-07-29 13:16:28 +1000

    putacknowleedge data fields renamed

--- v1/scripts/api.js
+++ v1/scripts/api.js
@@ -90,7 +90,7 @@ const API = {
             method: "POST",
             headers: API.login.headers,
             contentType: "application/json",
-            data: {firstname: signoff_first_name, lastname: signoff_last_name, role: signoff_role},
+            data: {signoff_first_name: signoff_first_name, signoff_last_name: signoff_last_name, signoff_role: signoff_role},
             success: function(result) {
                 if (API.handleResult(result)) {
                     console.log(result);
